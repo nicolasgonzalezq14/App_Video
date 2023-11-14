c0992eba9a3ff814afc57b668fcb9385adb3c75a
Nicolas Ignacio Gonzalez Quiñones <nicolasg495@gmail.com>
2023-11-13 21:49:09 -0300

    Orden Codigo

--- lib/models/movie_model.dart
+++ lib/models/movie_model.dart
@@ -25,8 +25,7 @@ class Movie {
     ),
     Movie(
       name: 'Rápidos y furiosos 10',
-      imagePath:
-          'https://i.imgur.com/x0uYMFJ.jpeg',
+      imagePath: 'https://i.imgur.com/x0uYMFJ.jpeg',
       videoPath: 'assets/videos/rapidoyfuriososx.mp4',
       category: 'Acción',
       year: 2022,
@@ -34,7 +33,8 @@ class Movie {
     ),
     Movie(
       name: 'El señor de los anillos',
-      imagePath: 'https://i0.wp.com/carlosjeguren.com/wp-content/uploads/2012/12/El-Se25C325B1or-de-los-Anillos-La-Comunidad-del-Anillo.jpg?ssl=1',
+      imagePath:
+          'https://i0.wp.com/carlosjeguren.com/wp-content/uploads/2012/12/El-Se25C325B1or-de-los-Anillos-La-Comunidad-del-Anillo.jpg?ssl=1',
       videoPath: 'assets/videos/elsenordelosanillos.mp4',
       category: 'Acción',
       year: 2001,
@@ -42,7 +42,8 @@ class Movie {
     ),
     Movie(
       name: 'Harry Potter y la piedra filosofal',
-      imagePath: 'https://larepublica.cronosmedia.glr.pe/original/2021/10/28/617b0722cd5ff4650728ef2d.jpg',
+      imagePath:
+          'https://larepublica.cronosmedia.glr.pe/original/2021/10/28/617b0722cd5ff4650728ef2d.jpg',
       videoPath: 'assets/videos/harrypotterylapiedrafilosofal.mp4',
       category: 'Cine de terror',
       year: 2001,
@@ -50,7 +51,8 @@ class Movie {
     ),
     Movie(
       name: 'Elementos',
-      imagePath: 'https://lumiere-a.akamaihd.net/v1/images/eu_xx_elemental_dplus_mob_m_60031c24.jpeg?region=0,0,1600,1200',
+      imagePath:
+          'https://lumiere-a.akamaihd.net/v1/images/eu_xx_elemental_dplus_mob_m_60031c24.jpeg?region=0,0,1600,1200',
       videoPath: 'assets/videos/elementos.mp4',
       category: 'Comedia Romántica',
       year: 2023,
